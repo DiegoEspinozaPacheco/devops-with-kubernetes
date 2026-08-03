bbb6ab060d194bd41e27edd2d6f7963718ab50c9
DiegoEspinozaPacheco <root@desphogar.gmail.com>
2026-08-02 22:42:03 -0400

    The project, step 3

---
+++ 1.5/main.py
@@ -0,0 +1,20 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<html><body><h1>Todo app</h1></body></html>")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+port = int(os.environ.get("PORT", 8000))
+
+if __name__ == "__main__":
+    print(f"Server started in port {port}", flush=True)
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
