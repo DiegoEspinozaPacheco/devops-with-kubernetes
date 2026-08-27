90d59f50b0e29767dfd12212a0de43d48a29dae5
DiegoEspinozaPacheco <root@desphogar.gmail.com>
2026-08-26 23:47:33 -0400

    2.3 Progress

---
+++ 2.3/ping-pong/main.py
@@ -0,0 +1,30 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+counter = 0
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global counter
+        if self.path == "/pingpong":
+            body = f"pong {counter}"
+            counter += 1
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(body.encode())
+        elif self.path == "/pings":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(str(counter).encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+port = int(os.environ.get("PORT", 3002))
+
+if __name__ == "__main__":
+    print(f"Server started in port {port}", flush=True)
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
